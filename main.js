aab2348443ab0fc9776b504f0e61f7e9b855a21c
FranPastori <franciele_pastori@hotmail.com>
2022-11-13 15:06:52 -0300

    melhorando a sintaxe

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-function tocaSom(idDoElementoAudio){
+function tocaSom(idDoElementoAudio) {
     document.querySelector(idDoElementoAudio).play();
 
 }
 
-const listaTeclas= document.querySelectorAll('.tecla');
+const listaTeclas = document.querySelectorAll('.tecla');
 
-var contador = 0 
+var contador = 0
 
-while (contador< listaTeclas.length) {
-    const botao = listaTeclas[contador];
+//while (contador< listaTeclas.length) {
+//    const botao = listaTeclas[contador];
+//    const instrumento = botao.classList[1];
+//    console.log(botao);
+//   console.log(instrumento);
+//    const idAudio = `#som_${instrumento}`;
+//    console.log(idAudio);
+//    botao.onclick = function () { tocaSom(idAudio);}
+//    contador++;
+//}
+
+for (let i = 0; i < listaTeclas.length; i++) {
+    const botao = listaTeclas[i];
     const instrumento = botao.classList[1];
     console.log(botao);
     console.log(instrumento);
     const idAudio = `#som_${instrumento}`;
     console.log(idAudio);
-    botao.onclick = function () { tocaSom(idAudio);}
-
-    contador++;
+    botao.onclick = function () { tocaSom(idAudio); }
 
 }
